e6a9e48fa7cb6b182d248a50116a3347e98d4025
yeylcode <yo911217@gmail.com>
2020-01-15 16:27:53 +0800

    #new springboot project#

---
+++ src/main/java/com/yeyl/jwiki/JwikiApplication.java
@@ -0,0 +1,13 @@
+package com.yeyl.jwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeyl/jwiki/JwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeyl.jwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
